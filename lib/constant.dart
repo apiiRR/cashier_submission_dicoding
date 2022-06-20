0a4b5836644bf452980279066065e3f5c5c93e18
apiiRR <ramadhanarafi437@gmail.com>
2022-06-20 19:09:13 +0700

    onboarding_screen

---
+++ lib/constant.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+const FontWeight bold = FontWeight.w700;
+const FontWeight light = FontWeight.w300;
+const FontWeight regular = FontWeight.w400;
+const FontWeight semiBold = FontWeight.w600;
+
+const primaryLineGrey = Color(0xFFBDBDBD);
+const primaryBlack = Color(0xFF212121);
+const primaryWhite = Color(0xFFFFFFFF);
+const primaryGrey = Color(0xFFD8D8D8);
+const tertiaryGrey = Color(0xFFF5F5F5);
+const primaryTosca = Color(0xFF1E7E8C);
+const primaryOrange = Color(0xFFFF8E01);
+const secondaryTosca = Color(0xFF9BC4C8);
+const secondaryOrange = Color(0xFFFFB14D);
+const tertiaryTosca = Color(0xFFDAECED);
+const tertiaryOrange = Color(0xFFFFF1DB);
+
+TextStyle bold12(colorText) {
+  return GoogleFonts.poppins(
+      textStyle: TextStyle(color: colorText, fontSize: 12, fontWeight: bold));
+}
+
+TextStyle light12(colorText) {
+  return GoogleFonts.poppins(textStyle: TextStyle(color: colorText, fontSize: 12, fontWeight: light));
+}
+
+TextStyle regular12(colorText) {
+  return GoogleFonts.poppins(textStyle: TextStyle(color: colorText, fontSize: 12, fontWeight: regular));
+}
+
+TextStyle semiBold12(colorText) {
+  return GoogleFonts.poppins(textStyle: TextStyle(color: colorText, fontSize: 12, fontWeight: semiBold));
+}
+
+TextStyle bold16(colorText) {
+  return GoogleFonts.poppins(textStyle: TextStyle(color: colorText, fontSize: 16, fontWeight: bold));
+}
+
+TextStyle light16(colorText) {
+  return GoogleFonts.poppins(textStyle: TextStyle(color: colorText, fontSize: 16, fontWeight: light));
+}
+
+TextStyle regular16(colorText) {
+  return GoogleFonts.poppins(textStyle: TextStyle(color: colorText, fontSize: 16, fontWeight: regular));
+}
+
+TextStyle semiBold16(colorText) {
+  return GoogleFonts.poppins(textStyle: TextStyle(color: colorText, fontSize: 16, fontWeight: semiBold));
+}
+
+TextStyle bold20(colorText) {
+  return GoogleFonts.poppins(textStyle: TextStyle(color: colorText, fontSize: 20, fontWeight: bold));
+}
+
+TextStyle light20(colorText) {
+  return GoogleFonts.poppins(textStyle: TextStyle(color: colorText, fontSize: 20, fontWeight: light));
+}
+
+TextStyle regular20(colorText) {
+  return GoogleFonts.poppins(textStyle: TextStyle(color: colorText, fontSize: 20, fontWeight: regular));
+}
+
+TextStyle semiBold20(colorText) {
+  return GoogleFonts.poppins(textStyle: TextStyle(color: colorText, fontSize: 20, fontWeight: semiBold));
+}
+
+TextStyle bold24(colorText) {
+  return GoogleFonts.poppins(textStyle: TextStyle(color: colorText, fontSize: 24, fontWeight: bold));
+}
+
+TextStyle light24(colorText) {
+  return GoogleFonts.poppins(textStyle: TextStyle(color: colorText, fontSize: 24, fontWeight: light));
+}
+
+TextStyle regular24(colorText) {
+  return GoogleFonts.poppins(textStyle: TextStyle(color: colorText, fontSize: 24, fontWeight: regular));
+}
+
+TextStyle semiBold24(colorText) {
+  return GoogleFonts.poppins(textStyle: TextStyle(color: colorText, fontSize: 24, fontWeight: semiBold));
+}
+
+TextStyle bold36(colorText) {
+  return GoogleFonts.poppins(
+      textStyle: TextStyle(color: colorText, fontSize: 36, fontWeight: bold));
+}
+
+TextStyle light36(colorText) {
+  return GoogleFonts.poppins(
+      textStyle: TextStyle(color: colorText, fontSize: 36, fontWeight: light));
+}
+
+TextStyle regular36(colorText) {
+  return GoogleFonts.poppins(
+      textStyle:
+          TextStyle(color: colorText, fontSize: 36, fontWeight: regular));
+}
+
+TextStyle semiBold36(colorText) {
+  return GoogleFonts.poppins(
+      textStyle:
+          TextStyle(color: colorText, fontSize: 36, fontWeight: semiBold));
+}
